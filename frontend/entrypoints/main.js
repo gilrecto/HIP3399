d73a2718e6945b0e9c19bb3a172bd940cd4b771a
Gil Recto Jr <grectojr@gmail.com>
2026-01-21 19:03:28 +0800

    Theme Build - initial commit

---
+++ frontend/entrypoints/main.js
@@ -0,0 +1,6 @@
+import 'vite/modulepreload-polyfill';
+
+// Custom scripts
+import '../web/scripts/hp_global_script.js';
+import '../web/scripts/hp_swiper_slider.js';
+import '../web/scripts/hp_accordion.js';
